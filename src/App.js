e1fc00b53b12f61a435fdbbed9eeca7d8772a275
Vamshi Krishna Reddy <30525566+vamshireddy09@users.noreply.github.com>
2022-11-18 11:07:58 +0530

    Content,Row

--- src/App.js
+++ src/App.js
@@ -2,7 +2,12 @@ import './App.css';
 import Nav from './components/Nav/Nav'
 import Header from './components/Header/Header';
 import Content from './components/Content/Content';
+import axios from 'axios';
 
+axios.defaults.baseURL = 'https://api.themoviedb.org/3';
+axios.defaults.params = {
+  api_key: 'fc599703459aae42aed33de718da32f9',
+};
 const App = () => {
   return (
     <div>
--- src/components/Content/Content.js
+++ src/components/Content/Content.js
@@ -1,4 +1,14 @@
+
 import './Content.css';
+import RowData from '../../constants/URLS';
+import Row from '../row/Row';
+
 export default function Content() {
-  return <div className='content-wrapper'>Content</div>;
+  return (
+    <div className='content-wrapper'>
+      {RowData.map((el) => {
+        return <Row title={el.title} url={el.url} />;
+      })}
+    </div>
+  );
 }
